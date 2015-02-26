9ac444b771bd987ee749e4aebe7e972516e9ca7a
David García Garzón <voki@canvoki.net>
2015-02-26 13:04:58 +0100

    script to accept modified b2b data

---
+++ acceptb2b.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+found=''
+for a in b2bdata/*-result.*; do 
+	[ -e $a ] || continue
+	mv $a ${a/-result/-expected}
+	found="$found $a"
+	echo "Accepting ${a}..."
+done
+if [ -z found ]
+then
+	echo "No s'han trobat resultats pendents d'acceptar"
+fi
+
+
