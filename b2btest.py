ab9ecd4bc615823a608475c41eb3a524368ea699
David García Garzón <voki@canvoki.net>
2015-03-04 18:21:32 +0100

    xml helper moved to b2btest

--- b2btest.py
+++ b2btest.py
@@ -1,6 +1,13 @@
 import unittest
 
+
 class TestCase(unittest.TestCase) :
+	"""
+	This class is yet quite adhoc for single case.
+	Paths should be configurable aswell as the way of composing
+	data file names.
+	"""
+
 	def assertBack2Back(self, result, testId) :
 		def write(result) :
 			with open(resultfilename,'w') as resultfile:
@@ -18,14 +25,32 @@ class TestCase(unittest.TestCase) :
 				expected=expectedfile.read()
 		except IOError as e:
 			write(result)
-			raise AssertionError("No expectation, accept with: mv {} {}".format(resultfilename, expectedfilename))
+			raise AssertionError(
+				"No expectation, accept with: mv {} {}".format(
+					resultfilename, expectedfilename))
 
 		self.maxDiff = None
 		try:
 			self.assertMultiLineEqual(expected, result)
 		except AssertionError:
 			import sys
-	 		print("Back-to-back results differ, accept with: mv {} {}".format(resultfilename, expectedfilename))
+			print(
+				"Back-to-back results differ, accept with: mv {} {}".format(
+					resultfilename, expectedfilename))
 			write(result)
 			raise
+
+def assertXmlEqual(self, got, want):
+	from lxml.doctestcompare import LXMLOutputChecker
+	from doctest import Example
+
+	checker = LXMLOutputChecker()
+	if checker.check_output(want, got, 0):
+		return
+	message = checker.output_difference(Example("", want), got, 0)
+	raise AssertionError(message)
+
+import unittest
+unittest.TestCase.assertXmlEqual = assertXmlEqual
+
 
