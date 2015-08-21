c81b0ab22ae6bb11a89d05157ebaf3e1c4e2a511
David García Garzón <david.garcia@upf.edu>
2015-08-21 22:20:39 +0200

    extracted query-sentRequests.sql

--- dbqueries.py
+++ dbqueries.py
@@ -4,8 +4,14 @@
 import psycopg2
 import psycopg2.extras
 import datetime
-
 from dbutils import *
+import os
+
+def loadQuery(name):
+	scriptPath=os.path.abspath(os.path.dirname(__file__))
+	fullFilename=os.path.join(scriptPath, name)
+	with open(fullFilename) as queryFile:
+		return queryFile.read()
 
 def idsProcessos(db):
 	with db.cursor() as cur:
@@ -579,74 +585,12 @@ def activatedRequests(db, inici, final, cursorManager=nsList):
 
 def sentRequests(db, inici, final, cursorManager=nsList) :
 	with db.cursor() as cur :
-		cur.execute("""\
-			SELECT
-				COUNT(*) AS nreq,
-				dist.id AS distriid,
-				dist.ref AS refdistribuidora,
-				tar.name as tarname,
-				provincia.code AS codiprovincia,
-				provincia.name AS nomprovincia,
-				dist.name AS distriname,
-				STRING_AGG(sw.id::text, ',' ORDER BY sw.id) AS casos
-			FROM
-				(
-				SELECT
-					id AS pass_id,
-					header_id,
-					1 AS process
-				FROM giscedata_switching_c1_01
-				WHERE
-					create_date >= %(periodStart)s AND
-					create_date < %(periodEnd)s AND
-					TRUE
-				UNION
-				SELECT
-					id AS pass_id,
-					header_id,
-					2 AS process
-				FROM giscedata_switching_c2_01
-				WHERE
-					create_date >= %(periodStart)s AND
-					create_date < %(periodEnd)s AND
-					TRUE
-				) AS step
-			LEFT JOIN
-				giscedata_switching_step_header AS sth ON step.header_id = sth.id
-			LEFT JOIN
-				giscedata_switching AS sw ON sw.id = sth.sw_id
-			LEFT JOIN
-				giscedata_cups_ps AS cups ON cups.id = sw.cups_id
-			LEFT JOIN
-				giscedata_polissa AS pol ON pol.id = sw.cups_polissa_id
-			LEFT JOIN
-				res_partner AS dist ON dist.id = pol.distribuidora
-			LEFT JOIN
-				giscedata_polissa_tarifa AS tar ON tar.id = pol.tarifa
-			LEFT JOIN
-				res_municipi ON res_municipi.id = cups.id_municipi
-			LEFT JOIN
-				res_country_state AS provincia ON provincia.id = res_municipi.state
-			GROUP BY
-				tar.name,
-				dist.name,
-				provincia.code,
-				dist.id,
-				dist.ref,
-				provincia.name,
-				dist.name,
-				TRUE
-			ORDER BY
-				tar.name,
-				dist.name,
-				provincia.code,
-				TRUE
-			"""
-			,dict(
+		cur.execute(
+			loadQuery('query-sentRequests.sql'),
+			dict(
 				periodStart = inici,
 				periodEnd = final,
 			))
-
 		result = cursorManager(cur)
 	return result
 
