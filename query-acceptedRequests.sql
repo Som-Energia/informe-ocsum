82a90e82f3871cbb962a88d8bc314efe683ec6c8
David García Garzón <david.garcia@upf.edu>
2015-09-01 03:38:47 +0200

    Accepted includes "priority 5" properly

--- query-acceptedRequests.sql
+++ query-acceptedRequests.sql
@@ -64,61 +64,114 @@ FROM (
 		END AS termini,
 		TRUE
 	FROM (
-		SELECT
-			id AS pass_id,
-			header_id,
-			'C3' AS tipo_cambio,
-			'c1' AS process,
-			data_acceptacio,
-			TRUE
-		FROM
-			giscedata_switching_c1_02
-		WHERE
-			data_acceptacio >= %(periodStart)s AND
-			data_acceptacio <= %(periodEnd)s AND
-			NOT rebuig AND
-			TRUE
+		/* c1_02, rebuig=false, accepted within the period */
+			SELECT
+				header_id,
+				'C3' AS tipo_cambio,
+				'c1' AS process,
+				data_acceptacio,
+				TRUE
+			FROM
+				giscedata_switching_c1_02
+			WHERE
+				data_acceptacio >= %(periodStart)s AND
+				data_acceptacio <= %(periodEnd)s AND
+				NOT rebuig AND
+				TRUE
 		UNION
-		SELECT
-			id AS pass_id,
-			header_id,
-			'C3' as tipo_cambio,
-			'c2' as process,
-			data_acceptacio,
-			TRUE
-		FROM
-			giscedata_switching_c2_02
-		WHERE
-			data_acceptacio >= %(periodStart)s AND
-			data_acceptacio <= %(periodEnd)s AND
-			NOT rebuig AND
-			TRUE
+		/* c2_02, rebuig=false, accepted within the period */
+			SELECT
+				header_id,
+				'C3' as tipo_cambio,
+				'c2' as process,
+				data_acceptacio,
+				TRUE
+			FROM
+				giscedata_switching_c2_02
+			WHERE
+				data_acceptacio >= %(periodStart)s AND
+				data_acceptacio <= %(periodEnd)s AND
+				NOT rebuig AND
+				TRUE
+		/* a3_02, rebuig=false, accepted within the period */
 /*
+			UNION
+			SELECT
+				header_id,
+				'C4' as tipo_cambio,
+				'a3' as process,
+				data_acceptacio,
+				TRUE
+			FROM
+				giscedata_switching_a3_02
+			WHERE
+				data_acceptacio >= %(periodStart)s AND
+				data_acceptacio <= %(periodEnd)s AND
+				NOT rebuig AND
+				TRUE
+*/
 		UNION
-		SELECT
-			id AS pass_id,
-			header_id,
-			'C4' as tipo_cambio,
-			'a3' as process,
-			data_acceptacio,
-			TRUE
-		FROM
-			giscedata_switching_a3_02
-		WHERE
-			data_acceptacio >= %(periodStart)s AND
-			data_acceptacio <= %(periodEnd)s AND
-			NOT rebuig AND
-			TRUE
-*/
-	/* TODO:
-	LEFT JOIN
-		crm_case AS case_ ON case_.id = sw.case_id
-		case_.priority=5 with pol.data_alta within the period
-*/
+		/* single 01 step, priority=5, polissa.data_alta en el periode */
+			SELECT
+				sth.id AS header_id,
+				step01.tipo_cambio AS tipo_cambio,
+				step01.process AS process,
+				pol.data_alta AS data_acceptacio,
+				TRUE
+			FROM
+				crm_case AS case_
+			LEFT JOIN
+				giscedata_switching AS sw ON sw.case_id = case_.id
+			LEFT JOIN
+				giscedata_switching_step_header AS sth ON sth.sw_id = sw.id
+			LEFT JOIN
+				giscedata_polissa AS pol ON pol.id = sw.cups_polissa_id
+			JOIN (
+				/* single step cases */
+				SELECT
+					sw.id,
+					count(sth.id) AS nsteps
+				FROM	
+					giscedata_switching AS sw
+				LEFT JOIN
+					giscedata_switching_step_header AS sth
+					ON sth.sw_id = sw.id
+				GROUP BY sw.id
+				HAVING count(sth.id) = 1
+			) AS swunic ON sw.id = swunic.id
+			JOIN (
+				/* and 01 steps */
+				SELECT
+					'C3' AS tipo_cambio,
+					'c1' AS process,
+					st.header_id as header_id
+				FROM
+					giscedata_switching_c1_01 AS st
+				UNION
+				SELECT
+					'C3' AS tipo_cambio,
+					'c2' AS process,
+					st.header_id as header_id
+				FROM
+					giscedata_switching_c2_01 AS st
+				UNION
+				SELECT
+					'C4' AS tipo_cambio,
+					'a3' AS process,
+					st.header_id as header_id
+				FROM
+					giscedata_switching_a3_01 AS st
+
+				) AS step01 ON step01.header_id = sth.id
+			WHERE
+				pol.data_alta >= %(periodStart)s AND
+				pol.data_alta <= %(periodEnd)s AND
+				case_.priority='5' AND
+				TRUE
 		) AS step
-	LEFT JOIN 
+	LEFT JOIN
 		giscedata_switching_step_header AS sth ON step.header_id = sth.id
-	LEFT JOIN 
+	LEFT JOIN
 		giscedata_switching AS sw ON sw.id = sth.sw_id
 	LEFT JOIN
 		giscedata_cups_ps AS cups ON sw.cups_id = cups.id
@@ -126,9 +179,9 @@ FROM (
 		res_municipi ON  cups.id_municipi = res_municipi.id
 	LEFT JOIN
 		res_country_state AS provincia ON res_municipi.state = provincia.id
-	LEFT JOIN 
+	LEFT JOIN
 		giscedata_polissa AS pol ON cups_polissa_id = pol.id
-	LEFT JOIN 
+	LEFT JOIN
 		res_partner AS dist ON pol.distribuidora = dist.id
 	LEFT JOIN
 		giscedata_polissa_tarifa AS tar ON pol.tarifa = tar.id
