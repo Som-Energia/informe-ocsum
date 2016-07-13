5e4066bed97e60240b94fa19eb7567e29adb6a69
David García Garzón <voki@canvoki.net>
2016-07-13 23:17:20 +0200

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "SwitchingReports",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
     scripts = [
         'atr_debugcase',
